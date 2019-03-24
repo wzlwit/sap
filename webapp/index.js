c39e53f23130ed4e4181fe7f1d137b3b395e31b9
Zhaolong Wang <zl.wangadfa@hotmail.com>
2019-03-23 23:44:04 -0400

    name change

--- webapp/index.js
+++ webapp/index.js
@@ -1,9 +1,27 @@
-sap.ui.define([
-	"sap/ui/core/mvc/XMLView"
-], function (XMLView) {
+/* sap.ui.define([
+    "sap/m/Button",
+    "sap/m/MessageToast"
+], function (Button, MessageToast) {
+    "use strict";
+
+    new Button({
+        text: "Ready...",
+        press: function () {
+            MessageToast.show("Hello World!");
+        }
+    }).placeAt("content");
+
+}); */
+
+sap.ui.getCore().attachInit(function () {
 	"use strict";
 
-	XMLView.create({viewName: "Quickstart.App"}).then(function (oView) {
-		oView.placeAt("content");
+    sap.ui.require([
+		"sap/ui/core/mvc/XMLView"
+	], function (XMLView) {
+		XMLView.create({viewName: "Quickstart.App"}).then(function (oView) {
+			oView.placeAt("content");
+		});
 	});
+
 });
