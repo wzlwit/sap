71dbd15a707b148efa7b16ca9847ccd25ba8210e
Zhaolong Wang <zl.wangadfa@hotmail.com>
2019-03-23 20:21:50 -0400

    index.js updated

--- webapp/index.js
+++ webapp/index.js
@@ -1,4 +1,4 @@
-sap.ui.define([
+/* sap.ui.define([
     "sap/m/Button",
     "sap/m/MessageToast"
 ], function (Button, MessageToast) {
@@ -10,5 +10,18 @@ sap.ui.define([
             MessageToast.show("Hello World!");
         }
     }).placeAt("content");
+
+}); */
+
+sap.ui.getCore().attachInit(function () {
+	"use strict";
+
+    sap.ui.require([
+		"sap/ui/core/mvc/XMLView"
+	], function (XMLView) {
+		XMLView.create({viewName: "Quickstart.App"}).then(function (oView) {
+			oView.placeAt("content");
+		});
+	});
 
 });
