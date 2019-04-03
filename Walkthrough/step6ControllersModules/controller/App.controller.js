01acbc1177311482c21fc9f04d4bbb6e649b7eda
Zhaolong Wang <1896216@johnabbottcollege.net>
2019-04-03 11:52:56 -0400

    step6

--- Walkthrough/step6ControllersModules/controller/App.controller.js
+++ Walkthrough/step6ControllersModules/controller/App.controller.js
@@ -1,15 +1,17 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
 
-		onShowHello : function () {
+		onShowHello: function () {
 			// show a native JavaScript alert
 			/* eslint-disable no-alert */
-			alert("Hello World");
+			//* alert("Hello World");
 			/* eslint-enable no-alert */
+			MessageToast.show("Hello World");
 		}
 	});
 
