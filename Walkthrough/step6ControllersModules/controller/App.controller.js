28378d95c52118dadc0358702e142b1dfe529bcb
Zhaolong Wang <1896216@johnabbottcollege.net>
2019-04-03 11:56:28 -0400

    comments

--- Walkthrough/step6ControllersModules/controller/App.controller.js
+++ Walkthrough/step6ControllersModules/controller/App.controller.js
@@ -12,6 +12,8 @@ sap.ui.define([
 			//* alert("Hello World");
 			/* eslint-enable no-alert */
 			MessageToast.show("Hello World");
+
+			// * We extend the array of required modules with the fully qualified path to sap.m.MessageToast. Once both modules, Controller and MessageToast, are loaded, the callback function is called and we can make use of both objects by accessing the parameters passed on to the function. 
 		}
 	});
 
