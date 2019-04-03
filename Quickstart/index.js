87f9c0f28d511ba03754748a317104b62ee0ec84
Zhaolong Wang <1896216@johnabbottcollege.net>
2019-04-03 11:05:41 -0400

    notes

--- Quickstart/index.js
+++ Quickstart/index.js
@@ -4,6 +4,7 @@ sap.ui.getCore().attachInit(function () {
     sap.ui.define([
 		"sap/ui/core/mvc/XMLView"
 	], function (XMLView) {
+		//using Quckstart/App.view.html to create an View
 		XMLView.create({viewName: "Quickstart.App"}).then(function (oView) {
 			oView.placeAt("content");
 		});
